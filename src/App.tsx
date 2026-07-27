b4aa745eed1494da656c30133d044cc4695c6741
Klar <klar@livinglogic.de>
2026-07-27 15:02:17 +0000

    Klar Auto-Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorBusProvider } from '@/components/ErrorBus';
 import { Layout } from '@/components/Layout';
 import DashboardOverview from '@/pages/DashboardOverview';
-import { WorkflowPlaceholders } from '@/components/WorkflowPlaceholders';
 import AdminPage from '@/pages/AdminPage';
 import WohnungenPage from '@/pages/WohnungenPage';
 import FritzBoxenPage from '@/pages/FritzBoxenPage';
@@ -29,7 +28,7 @@ export default function App() {
               {/* <public:routes> */}
               {/* </public:routes> */}
               <Route element={<Layout />}>
-                <Route index element={<><div className="mb-8"><WorkflowPlaceholders /></div><DashboardOverview /></>} />
+                <Route index element={<DashboardOverview />} />
                 <Route path="wohnungen" element={<WohnungenPage />} />
                 <Route path="fritz!boxen" element={<FritzBoxenPage />} />
                 <Route path="admin" element={<AdminPage />} />
